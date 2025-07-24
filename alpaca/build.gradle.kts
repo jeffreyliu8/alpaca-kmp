abdf63c0c3fb08c9e0d7ddc78ed3abc92a22a766
Jeffrey Liu <jeffreyliu8@gmail.com>
2025-07-24 00:59:10 -0700

    Update Maven publish plugin version and remove unused SonatypeHost import

--- alpaca/build.gradle.kts
+++ alpaca/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
@@ -129,7 +128,7 @@ kotlin {
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral()
 
     signAllPublications()
 
